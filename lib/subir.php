e505708ca682e5fd5265c1739bde4993a31bae5b
Arturo Murillo <amurillop90@gmail.com>
2013-06-07 09:15:35 -0430

    Initial CSV

--- lib/subir.php
+++ lib/subir.php
@@ -1,6 +1,8 @@
 <?
+    use  SimpleExcel\SimpleExcel;
     session_start();
     require "cAutorizacion.php";
+    require "SimpleCSV/src/SimpleExcel/SimpleExcel.php";
     extract($_GET);
     extract($_POST);
 
@@ -44,6 +46,27 @@
       	echo "Type: " . $_FILES["file"]["type"] . "<br>";
       	echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
       	echo "Stored in: " . $_FILES["file"]["tmp_name"];
+
+    if (file_exists("../files/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "../files/" . $_FILES["file"]["name"]);
+      echo "<br>Stored in: " . "../files/" . $_FILES["file"]["name"];
+      }
+
+      $file = "../files/" . $_FILES["file"]["name"];
+
+      $excel = new SimpleExcel('CSV');
+      $excel->parser->loadFile($file);  // Load CSV file
+
+      echo $excel->parser->getRow(2)[0];
+
+      unlink($file);
+
     }else{
       //header("Location: ../SubirArchivo.php?error"); 
     }

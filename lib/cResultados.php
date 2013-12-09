9492c38974affc091062a07473882ed656eeef7a
Jerilyn Goncalves <jerilyngoncalves@gmail.com>
2013-12-09 18:29:48 -0430

    Cambios en Ver Resultados. Se pueden ver los resultados de los evaluadores y los detalles de la evaluacion

--- lib/cResultados.php
+++ lib/cResultados.php
@@ -10,35 +10,158 @@
     require_once 'XML/RPC2/Client.php';
     
     if(isset($_GET['token_ls'])){
-    
-      $sql="SELECT id_encuesta FROM PERSONA_ENCUESTA WHERE token_ls='".$_GET['token_ls']."'";
-      $atts = array("id_encuesta");
-      $resultado= obtenerDatos($sql, $conexion, $atts, "Enc"); //ID de la encuesta para el token del usuario
       
-      $id_encuesta=$resultado['Enc']['id_encuesta'][0];
-      $sql="SELECT id_pregunta, titulo FROM PREGUNTA WHERE id_encuesta='".$id_encuesta."' AND seccion='competencia'";
+      //Obtención del ID para: encuesta, usuario evaluador, cargo evaluado, unidad asociada
+      $sql="SELECT id_encuesta, id_evaluado, id_car, id_unidad FROM PERSONA_ENCUESTA WHERE token_ls='".$_GET['token_ls']."'";
+      $atts = array("id_encuesta", "id_evaluado", "id_car", "id_unidad");
+      $resultado= obtenerDatos($sql, $conexion, $atts, "Enc"); 
+     
+      $id_evaluado=$resultado['Enc']['id_evaluado'][0];//ID del usuario evaluado
+      //Obtención del nombre del evaluado
+      $sql="SELECT nombre, apellido, cedula FROM PERSONA WHERE id='".$id_evaluado."'";
+      $atts = array("nombre", "apellido", "cedula");
+      $aux= obtenerDatos($sql, $conexion, $atts, "Nom");
+      $NOMBRE=$aux['Nom']['nombre'][0].' '.$aux['Nom']['apellido'][0]; //Nombre y apellido del usuario
+      $CEDULA=$aux['Nom']['cedula'][0];
+      
+      $id_car=$resultado['Enc']['id_car'][0];//ID del cargo evaluado
+      //Obtención del nombre del cargo evaluado
+      $sql="SELECT nombre FROM CARGO WHERE id='".$id_car."'";
+      $atts = array("nombre");
+      $aux= obtenerDatos($sql, $conexion, $atts, "Car");
+      $CARGO=$aux['Car']['nombre'][0]; //Nombre del cargo
+      
+      $id_unidad=$resultado['Enc']['id_unidad'][0];//ID de la unidad adscrita del evaluado
+      //Obtención del nombre de la unidad a la que está adscrito el usuario
+      $sql="SELECT nombre FROM ORGANIZACION WHERE id='".$id_unidad."'";
+      $atts = array("nombre");
+      $aux= obtenerDatos($sql, $conexion, $atts, "Org");
+      $UNIDAD=$aux['Org']['nombre'][0]; //Nombre de la unidad
+      
+      $id_encuesta=$resultado['Enc']['id_encuesta'][0];//ID de la encuesta para el token del usuario
+      
+      //Obtención de las preguntas de la encuesta
+      $sql="SELECT id_pregunta, titulo FROM PREGUNTA WHERE id_encuesta='".$id_encuesta."' AND seccion='competencia' AND id_pregunta_root_ls IS NOT NULL ORDER BY id_pregunta";
       $atts = array("id_pregunta", "titulo", "resultado");
       $LISTA_COMPETENCIAS= obtenerDatos($sql, $conexion, $atts, "Preg"); //Lista de preguntas de la sección de competencias
-      $sql="SELECT id_pregunta, titulo FROM PREGUNTA WHERE id_encuesta='".$id_encuesta."' AND seccion='factor'";
+      $sql="SELECT id_pregunta, titulo FROM PREGUNTA WHERE id_encuesta='".$id_encuesta."' AND seccion='factor' AND id_pregunta_root_ls IS NOT NULL ORDER BY id_pregunta";
       $LISTA_FACTORES= obtenerDatos($sql, $conexion, $atts, "Preg"); //Lista de preguntas de la sección de competencias
 
+      //Obtención de resultados para la sección de competencias
       for($i=0; $i<$LISTA_COMPETENCIAS[max_res] ;$i++){
 	$id_pregunta_i=$LISTA_COMPETENCIAS['Preg']['id_pregunta'][$i];
-	$sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_i."'";
+	$sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_i."' AND token_ls='".$_GET['token_ls']."'";
 	$atts = array("respuesta");
 	$aux= obtenerDatos($sql, $conexion, $atts, "Res");
 	$LISTA_COMPETENCIAS['Preg']['resultado'][$i]=$aux['Res']['respuesta'][0];
       }
       
+      //Obtención de resultados para la sección de factores
       for($i=0; $i<$LISTA_FACTORES[max_res] ;$i++){
 	$id_pregunta_i=$LISTA_FACTORES['Preg']['id_pregunta'][$i];
-	$sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_i."'";
+	$sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_i."' AND token_ls='".$_GET['token_ls']."'";
 	$atts = array("respuesta");
 	$aux= obtenerDatos($sql, $conexion, $atts, "Res");
 	$LISTA_FACTORES['Preg']['resultado'][$i]=$aux['Res']['respuesta'][0];
       }
+      
+      //Obtención del ID y token de cada evaluador
+      $sql="SELECT id_encuestado, token_ls FROM PERSONA_ENCUESTA WHERE id_encuesta='".$id_encuesta."' AND tipo='evaluador' AND estado='Finalizada'";
+      $atts = array("id_encuestado", "token_ls", "nombre", "re_competencia", "re_factor");
+      $LISTA_EVALUADORES=obtenerDatos($sql, $conexion, $atts, "Eva");
+      $PROMEDIO_EVALUADORES=array("re_competencia", "re_factor");//Arreglo donde se lleva la suma de los resultados de los evaluadores
+      
+      //Obtener respuestas de los evaluadores
+      for($i=0; $i<$LISTA_EVALUADORES[max_res]; $i++){
+	
+	$sql="SELECT nombre, apellido FROM PERSONA WHERE id='".$LISTA_EVALUADORES['Eva']['id_encuestado'][$i]."'";
+	$atts=array("nombre", "apellido");
+	$aux= obtenerDatos($sql, $conexion, $atts, "Nom");
+	$LISTA_EVALUADORES['Eva']['nombre'][$i]=$aux['Nom']['nombre'][0].' '.$aux['Nom']['apellido'][0];
+	$LISTA_EVALUADORES['Eva']['re_competencia'][$i]=array();
+	$LISTA_EVALUADORES['Eva']['re_factor'][$i]=array();
+	
+	//Obtener resultados para la sección de competencias
+	for ($j=0; $j<$LISTA_COMPETENCIAS[max_res] ;$j++){
+	  $id_pregunta_j=$LISTA_COMPETENCIAS['Preg']['id_pregunta'][$j];
+	  $sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_j."' AND token_ls='".$LISTA_EVALUADORES['Eva']['token_ls'][$i]."'";
+	  $atts = array("respuesta");
+	  $aux= obtenerDatos($sql, $conexion, $atts, "Res");
+	  $LISTA_EVALUADORES['Eva']['re_competencia'][$i][$j]=$aux['Res']['respuesta'][0];
+	  
+	  switch($aux['Res']['respuesta'][0]){
+	  
+	    case 'Siempre':
+	      if(isset($PROMEDIO_EVALUADORES['re_competencia'][$j])) {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=($PROMEDIO_EVALUADORES['re_competencia'][$j]+3);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=3;
+	      }
+	      break;
+	    case 'Casi siempre':
+	      if(isset($PROMEDIO_EVALUADORES['re_competencia'][$j])) {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=($PROMEDIO_EVALUADORES['re_competencia'][$j]+2);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=2;
+	      }
+	      break;
+	    case 'Pocas veces':
+	      if(isset($PROMEDIO_EVALUADORES['re_competencia'][$j])) {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=($PROMEDIO_EVALUADORES['re_competencia'][$j]+1);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=1;
+	      }
+	      break;;
+	    case 'Nunca':
+	      if(!(isset($PROMEDIO_EVALUADORES['re_competencia'][$j]))) {
+		$PROMEDIO_EVALUADORES['re_competencia'][$j]=0;
+	      }
+	      break;
+	  }//cierre del switch
+	}//cierre del for
+	
+	//Obtener resultados para la sección de factores
+	for ($j=0; $j<$LISTA_FACTORES[max_res] ;$j++){
+	  $id_pregunta_j=$LISTA_FACTORES['Preg']['id_pregunta'][$j];
+	  $sql="SELECT respuesta FROM RESPUESTA WHERE id_pregunta='".$id_pregunta_j."' AND token_ls='".$LISTA_EVALUADORES['Eva']['token_ls'][$i]."'";
+	  $atts = array("respuesta");
+	  $aux= obtenerDatos($sql, $conexion, $atts, "Res");
+	  $LISTA_EVALUADORES['Eva']['re_factor'][$i][$j]=$aux['Res']['respuesta'][0];
+	  
+	  switch($aux['Res']['respuesta'][0]){
+	    case 'Excelente':
+	      if(isset($PROMEDIO_EVALUADORES['re_factor'][$j])) {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=($PROMEDIO_EVALUADORES['re_factor'][$j]+3);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=3;
+	      }
+	      break;
+	    case 'Sobre lo esperado':
+	      if(isset($PROMEDIO_EVALUADORES['re_factor'][$j])) {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=($PROMEDIO_EVALUADORES['re_factor'][$j]+2);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=2;
+	      }
+	      break;
+	    case 'En lo esperado':
+	      if(isset($PROMEDIO_EVALUADORES['re_factor'][$j])) {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=($PROMEDIO_EVALUADORES['re_factor'][$j]+1);
+	      } else {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=1;
+	      }
+	      break;;
+	    case 'Por debajo de lo esperado':
+	      if(!(isset($PROMEDIO_EVALUADORES['re_factor'][$j]))) {
+		$PROMEDIO_EVALUADORES['re_factor'][$j]=0;
+	      }
+	      break;
+	  }//cierre del switch
+	}//cierre de iteración sobre factores
+      }//cierre iteración sobre evaluadores
+      
+      
      
-    }
+    }//cierre del if
     
     //Cierre conexión a la BD
     cerrarConexion($conexion);
--- vResultados.php
+++ vResultados.php
@@ -3,7 +3,6 @@
     $Legend = "Resultados de la Evaluación";
     include "lib/cResultados.php";
     include "vHeader.php";
-    //require_once "lib/phpChart_Lite/conf.php";
     extract($_GET);
     extract($_POST);
     $all = true;
@@ -29,60 +28,122 @@
 </script> 
 
 
+  <!--ENCABEZADO REPORTE DE RESULTADOS-->
+  <br>
+  <p class="lsmall muted"> Datos del trabajador evaluado</p>
+  <br>
+  <div class="row">
+    <div class="span1" align="right">
+      <img src="./img/iconos/user-new.jpg" class="img-circle">
+    </div>
+    <div class="span11">
+    <blockquote>
+       <p><?echo $NOMBRE?></p>
+       <p class="lsmall">C.I. <? echo $CEDULA?></p>
+       <p class="lsmall"><? echo $CARGO?></p>
+       <p class="lsmall"><? echo $UNIDAD?></p>
 
+    </blockquote>
+    </div>
+  </div>
+  <br>
+  
   <!--RESULTADOS PARA LA SECCION DE COMPETENCIAS-->
   <p class="lead"><small>Evaluación de competencias</small></p>
   <p class="lsmall muted"> Resultados obtenidos para la evaluación de competencias</p>
   
     <div class="row">
     <div class="span1"></div>
-    <div class="span10">
-    <br>
+    <div class="span10"><br>
+    
     <table class="tabla_competencias" style="display: none">
     <caption>Prueba de título</caption>
-    <thead>
-      <tr>
+    <thead><tr>
 	<td></td>
-	<?php
-	  for($i=1; $i<$LISTA_COMPETENCIAS['max_res']-2; $i++){
-	?>
-	  <th scope="row">C<? echo $i;?></th>
-	<?php
-	  }
-	?>
-      </tr>
-    </thead>
+	<?php for($i=0; $i<$LISTA_COMPETENCIAS['max_res']; $i++){ ?>
+	  <th scope="row">C<? echo $i+1;?></th>
+	<?php } ?>
+    </tr></thead>
+    
     <tbody>
-	<tr>
+      <tr>
 	    <th scope="col">Auto-evaluación</th>
-	    <?php
-	      for($i=1; $i<$LISTA_COMPETENCIAS['max_res']-2; $i++){
-		switch($LISTA_COMPETENCIAS['Preg']['resultado'][$i]){
-		  case 'Siempre':
-		    echo  '<td>3</td>';
-		    break;
-		  case 'Casi siempre':
-		    echo  '<td>2</td>';
-		    break;
-		  case 'Pocas veces':
-		    echo  '<td>1</td>';
-		    break;
-		  case 'Nunca':
-		    echo  '<td>0</td>';
-		    break;
-		}//cierra switch
-	      }//cierra iteración
-	    ?>
-	</tr>
-
+	    <?php for($i=0; $i<$LISTA_COMPETENCIAS['max_res']; $i++){
+	      switch($LISTA_COMPETENCIAS['Preg']['resultado'][$i]){
+	      case 'Siempre':
+		echo  '<td>3</td>';
+		break;
+	      case 'Casi siempre':
+		echo  '<td>2</td>';
+		break;
+	      case 'Pocas veces':
+		echo  '<td>1</td>';
+		break;
+	      case 'Nunca':
+		echo  '<td>0</td>';
+		break;
+	      }//cierra switch
+	    }?>
+      </tr>
+      <tr>
+	  <th scope="col">Evaluación del supervisor inmediato</th>
+	  <?php 
+	    $n=count($LISTA_EVALUADORES['Eva']['id_encuestado']);
+	    for($i=0; $i<count($PROMEDIO_EVALUADORES['re_competencia']); $i++){
+	      echo '<td>'.($PROMEDIO_EVALUADORES['re_competencia'][$i]/$n).'</td>';
+	    }
+	  ?>
+      <td>3</td><!--Truco para definir el máximo-->
+      </tr>
+      
     </tbody>
     </table>
+    
     </div> <!--Cierre span10-->
     <div class="span1"></div>
-    </div> <!-- Cierre row-->
+    </div><br><br><!-- Cierre row-->
+    
+    <!--Tabla de detalles-->
+    <table class="table table-hover" style="margin-left: 0;">
+      <thead>
+	<tr>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd"><small>Competencia</small></th>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd">
+	    <small>Resultado auto-evaluación</small>
+	    <span style="font-size:8px; padding-left:8px; background:#62c462;">&nbsp;</span>
+	  </th>
+	  <? for ($j=0; $j<count($LISTA_EVALUADORES['Eva']['nombre']); $j++){
+	    echo "<th class='lsmallT' style='border-top: 1px solid #dddddd'>";
+	    echo "<small>Resultado evaluación<br>(".$LISTA_EVALUADORES['Eva']['nombre'][$j].") </small>";
+	    echo "<span style='font-size:8px; padding-left:8px; background:#0088cc;'>&nbsp;</span></th>";
+	  } ?>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd"><small>Resultado esperado</small></th>
+	</tr>
+      </thead>
+      <tbody role="alert" aria-live="polite" aria-relevant="all">   
+      <!-- Listado de evaluaciones finalizadas -->
+      <?php
+	for ($i=0;$i<$LISTA_COMPETENCIAS['max_res'];$i++){
+      ?>
+	<tr class="<?php echo $color_tabla; ?>" >
+	  <!--Competencia-->
+	  <td class="center lsmallT" nowrap><small><? echo $LISTA_COMPETENCIAS['Preg']['titulo'][$i]." (C".($i+1).")";?></small></td>  
+	  <!--Resultado auto-evaluación-->
+	  <td class="center lsmallT" nowrap><small><? echo $LISTA_COMPETENCIAS['Preg']['resultado'][$i]?></small></td>    
+	  <!--Resultado de las evaluaciones-->
+	  <? for ($j=0; $j<count($LISTA_EVALUADORES['Eva']['id_encuestado']); $j++){
+	    echo "<td class='center lsmallT' nowrap><small>".$LISTA_EVALUADORES['Eva']['re_competencia'][$j][$i]."</small></td>";
+	  } ?>
+	  <!--Resultado esperado-->
+	  <td class="center lsmallT" nowrap><small><? echo "Siempre"?></small></td>
+	</tr>
+      <? } //cierre del for
+      ?>   
+      </tbody>
+    </table>
+    <!--FIN DE LA SECCION DE COMPETENCIAS-->
     
     <!--RESULTADOS PARA LA SECCION DE FACTORES-->
-    <!--ARREGLAR!!! URGENTE!!!-->
     <br>
     <p class="lead"><small>Evaluación de factores</small></p>
     <p class="lsmall muted"> Resultados obtenidos para la evaluación de factores desempeño</p>
@@ -96,9 +157,9 @@
       <tr>
 	<td></td>
 	<?php
-	  for($i=1; $i<$LISTA_FACTORES['max_res']-5; $i++){
+	  for($i=0; $i<$LISTA_FACTORES['max_res']; $i++){
 	?>
-	  <th scope="row">F<? echo $i;?></th>
+	  <th scope="row">F<? echo $i+1;?></th>
 	<?php
 	  }
 	?>
@@ -108,7 +169,7 @@
 	<tr>
 	    <th scope="col">Auto-evaluación</th>
 	    <?php
-	      for($i=1; $i<$LISTA_FACTORES['max_res']-5; $i++){
+	      for($i=0; $i<$LISTA_FACTORES['max_res']; $i++){
 		switch($LISTA_FACTORES['Preg']['resultado'][$i]){
 		  case 'Excelente':
 		    echo  '<td>3</td>';
@@ -126,13 +187,63 @@
 	      }//cierra iteración
 	    ?>
 	</tr>
-
+	<tr>
+	  <th scope="col">Evaluación del supervisor inmediato</th>
+	  <?php 
+	    $n=count($LISTA_EVALUADORES['Eva']['id_encuestado']);
+	    for($i=0; $i<count($PROMEDIO_EVALUADORES['re_factor']); $i++){
+	      echo '<td>'.($PROMEDIO_EVALUADORES['re_factor'][$i]/$n).'</td>';
+	    }
+	  ?>
+	<td>3</td><!--Truco para definir el máximo-->
+	</tr>
+	
     </tbody>
     </table>
     </div> <!--Cierre span10-->
     <div class="span1"></div>
-    </div> <!-- Cierre row-->
+    </div><br><br> <!-- Cierre row-->
     
+    <!--Tabla de detalles-->
+    <table class="table table-hover" style="margin-left: 0;">
+      <thead>
+	<tr>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd"><small>Factor de desempeño</small></th>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd">
+	    <small>Resultado auto-evaluación</small>
+	    <span style="font-size:8px; padding-left:8px; background:#62c462;">&nbsp;</span>
+	  </th>
+	  <? for ($j=0; $j<count($LISTA_EVALUADORES['Eva']['nombre']); $j++){
+	    echo "<th class='lsmallT' style='border-top: 1px solid #dddddd'>";
+	    echo "<small>Resultado evaluación<br>(".$LISTA_EVALUADORES['Eva']['nombre'][$j].") </small>";
+	    echo "<span style='font-size:8px; padding-left:8px; background:#0088cc;'>&nbsp;</span></th>";
+	  } ?>
+	  <th class="lsmallT" style="border-top: 1px solid #dddddd"><small>Resultado esperado</small></th>
+	</tr>
+      </thead>
+      <tbody role="alert" aria-live="polite" aria-relevant="all">   
+      <!-- Listado de evaluaciones finalizadas -->
+      <?php
+	for ($i=0;$i<$LISTA_FACTORES['max_res'];$i++){
+      ?>
+	<tr class="<?php echo $color_tabla; ?>" >
+	  <!--Competencia-->
+	  <td class="center lsmallT" style="width: 40%"><small><? echo $LISTA_FACTORES['Preg']['titulo'][$i]." (F".($i+1).")";?></small></td>  
+	  <!--Resultado auto-evaluación-->
+	  <td class="center lsmallT" nowrap><small><? echo $LISTA_FACTORES['Preg']['resultado'][$i]?></small></td>    
+	  <!--Resultado de las evaluaciones-->
+	  <? for ($j=0; $j<count($LISTA_EVALUADORES['Eva']['id_encuestado']); $j++){
+	    echo "<td class='center lsmallT' nowrap><small>".$LISTA_EVALUADORES['Eva']['re_factor'][$j][$i]."</small></td>";
+	  } ?>
+	  <!--Resultado esperado-->
+	  <td class="center lsmallT" nowrap><small><? echo "Excelente"?></small></td>
+	</tr>
+      <? } //cierre del for
+      ?>   
+      </tbody>
+    </table>
+    <!--FIN DE LA SECCION DE FACTORES-->
+   
 <?
 include "vFooter.php";
 ?>
